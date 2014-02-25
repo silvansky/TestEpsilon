d640ba4dd0255f57e55945bbaf37937a04fc99bd
silvansky <v.silvansky@gmail.com>
2014-02-25 16:00:39 +0400

    Initial version

--- TestEpsilon/VSViewController.m
+++ TestEpsilon/VSViewController.m
@@ -8,6 +8,9 @@
 
 #import "VSViewController.h"
 
+#include <float.h>
+#import <ImageIO/ImageIO.h>
+
 @interface VSViewController ()
 
 @end
@@ -16,14 +19,23 @@
 
 - (void)viewDidLoad
 {
+	float x = 1.f;
+	x += 10.f;
+	NSLog(@"%f", x);
+	x -= 10.f;
+	if (x < FLT_EPSILON)
+	{
+		x = 1.f / 3.f;
+	}
+	NSNumber *n = @(x);
+	NSLog(@"%@: %d, %d", n, [n intValue], (int)[n floatValue]);
+	self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"head"]];
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
